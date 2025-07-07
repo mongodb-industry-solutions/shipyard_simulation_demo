7d15276a09f000fe7e8ad1c7aeb78c40ad19402e
RamiPinto <rami.pinto@mongodb.com>
2025-07-07 14:58:34 +0200

    Deployment complete

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const backendHost = process.env.BACKEND_HOST || "localhost";
+
+const nextConfig = {
+  rewrites: async () => {
+    return [
+      {
+        source: "/api-rewrite/:path*",
+        destination: `http://${backendHost}:8008/:path*`,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
